2b8a03955f36e9d317a6a8536b33cfc7e3ddbb6f
maris.ancans <maris.ancans@applyit.lv>
2018-10-05 18:53:23 +0300

    Demo app

---
+++ cmake-build-debug/CMakeFiles/imguitest.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/maris/CLionProjects/imguitest/src/imgui/imgui.cpp" "/home/maris/CLionProjects/imguitest/cmake-build-debug/CMakeFiles/imguitest.dir/src/imgui/imgui.cpp.o"
+  "/home/maris/CLionProjects/imguitest/src/imgui/imgui_demo.cpp" "/home/maris/CLionProjects/imguitest/cmake-build-debug/CMakeFiles/imguitest.dir/src/imgui/imgui_demo.cpp.o"
+  "/home/maris/CLionProjects/imguitest/src/imgui/imgui_draw.cpp" "/home/maris/CLionProjects/imguitest/cmake-build-debug/CMakeFiles/imguitest.dir/src/imgui/imgui_draw.cpp.o"
+  "/home/maris/CLionProjects/imguitest/src/imgui/imgui_impl_glfw.cpp" "/home/maris/CLionProjects/imguitest/cmake-build-debug/CMakeFiles/imguitest.dir/src/imgui/imgui_impl_glfw.cpp.o"
+  "/home/maris/CLionProjects/imguitest/src/imgui/imgui_impl_opengl3.cpp" "/home/maris/CLionProjects/imguitest/cmake-build-debug/CMakeFiles/imguitest.dir/src/imgui/imgui_impl_opengl3.cpp.o"
+  "/home/maris/CLionProjects/imguitest/src/imgui/imgui_widgets.cpp" "/home/maris/CLionProjects/imguitest/cmake-build-debug/CMakeFiles/imguitest.dir/src/imgui/imgui_widgets.cpp.o"
+  "/home/maris/CLionProjects/imguitest/src/main.cpp" "/home/maris/CLionProjects/imguitest/cmake-build-debug/CMakeFiles/imguitest.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLFW_DLL"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/imguitest.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imguitest.dir/src/main.cpp.o"
+  "CMakeFiles/imguitest.dir/src/imgui/imgui.cpp.o"
+  "CMakeFiles/imguitest.dir/src/imgui/imgui_demo.cpp.o"
+  "CMakeFiles/imguitest.dir/src/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/imguitest.dir/src/imgui/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/imguitest.dir/src/imgui/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/imguitest.dir/src/imgui/imgui_widgets.cpp.o"
+  "imguitest.pdb"
+  "imguitest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/imguitest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
